571a193eb57ce513e2b858513dd3e2549fd01b57
Robeil Aregawi <65261399+Robeil@users.noreply.github.com>
2020-07-30 14:45:47 -0700

    adding another ajax call

--- script.js
+++ script.js
@@ -4,67 +4,13 @@ $(document).ready(function () {
 
     var citties = [];
     // to retrive the city
-    //var cittyArr = JSON.parse(localStorage.getItem("cities"));
+    var cittyArr = JSON.parse(localStorage.getItem("cities"));
     //
-    var cityArr = JSON.parse(localStorage.getItem("Cities"));
+    //var cityArr = JSON.parse(localStorage.getItem("Cities"));
 
     // Variable stating index of city in cityArr
     var cityIndex = "";
-
-    // Today's date
-    var today = new Date();
-
-    // Variable date for 5 day forecast (day)
-    var fiveDay = "";
-
-    // Variable date for 5 day forecast (month)
-    var fiveMonth = "";
-
-    // Variable date for 5 day forecast (year)
-    var fiveYear = "";
-
-    var date = {
-        day: today.getDate(),
-        month: (today.getMonth() + 1),
-        year: today.getUTCFullYear()
-    }
-
-    // Formatted today's date
-    var todayDate = date.month + "/" + date.day + "/" + date.year;
-
-    // function to display clickable buttons with city names in aside tag
-    function displayBtns() {
-
-        $("#button-container").empty();
-
-        for (i = 0; i < cityArr.length; i++) {
-
-            // Creates city button for aside tag
-            var cityBtn = $("<button>");
-
-            // Creates city button remove selector 'x'
-            var removeBtn = $("<i>");
-
-            cityBtn.addClass("city-buttons btn");
-
-            removeBtn.addClass("fa fa-times");
-
-            removeBtn.attr("aria-hidden", "true");
-
-            cityBtn.attr("data-name", cityArr[i]);
-
-            removeBtn.attr("data-name", cityArr[i]);
-
-            cityBtn.text(cityArr[i]);
-
-            cityBtn.append(removeBtn);
-
-            $("#button-container").append(cityBtn);
-
-        }
-
-    }
-
+   
     //var city = "";
     //
     var searchButton = document.getElementById("search-button");
@@ -74,6 +20,7 @@ $(document).ready(function () {
     function displayWeather(event) {
         event.preventDefault();
         var city = $("#city").val().trim();
+
         // first ajax call to get city data
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apikey + "&units=imperial"
         $.ajax({
@@ -88,33 +35,28 @@ $(document).ready(function () {
             $("#weather-display").append(img);
             $(".name").text(response.name);
             $(".date").text(new Date(response.dt * 1000).toLocaleDateString())
+
+
+        })
             //queryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?" + response.city + "&appid=" + apikey + "&units=imperial"
-            queryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + response.coord.lat + "&lon=" + response.coord.lon + "&appid=" + apikey + "&units=imperial"
-            console.log(img);
+            queryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + "&lon="+ "&appid=" + apikey
         
+                $.ajax({
+                    url: queryURL,
+                    method: "GET"
+                }).then(function (response) {
+                    console.log(response);
         
+                    iconurl = "http://openweathermap.org/img/w/" + response.weather.main + ".png";
+                    img.attr("src", iconurl)
+                    $("#weather-display").append(img);
+                    $(".main").text(response.main);
+                    $(".date").text(new Date(response.dt * 1000).toLocaleDateString())
+                    //uvURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIkey + "&lat=" + response.coord.lat + "&lon=" + response.coord.lon;
         
-        })
-
-
-        // 
-/*
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
-
-            iconurl1 = "http://openweathermap.org/img/w/" + response.weather.main
-            img.attr("src", iconurl)
-            $("#weather-display").append(img);
-            $(".main").text(response.main);
-            $(".date").text(new Date(response.dt * 1000).toLocaleDateString())
-            //uvURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIkey + "&lat=" + response.coord.lat + "&lon=" + response.coord.lon;
-
-            queryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?" + response.coord.name + "&appid=" + apikey
-        })
-*/
+                  //  queryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?" + response.coord.name + "&appid=" + apikey
+                })
+        
 
     };
 
