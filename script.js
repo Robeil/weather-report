4061b0a8cbd1f53273e7c216b9e95753a43e1693
Robeil Aregawi <65261399+Robeil@users.noreply.github.com>
2020-08-02 12:27:36 -0700

    appending the data to the html file

--- script.js
+++ script.js
@@ -1,24 +1,80 @@
-$(document).ready(function () {
+$(document).ready(function() {
     // apiKey 
     var apikey = "fe0f799b6721a0dd5bd4752f1b8feb31";
 
-    var citties = [];
-    // to retrive the city
-    var cittyArr = JSON.parse(localStorage.getItem("cities"));
-    //
-    //var cityArr = JSON.parse(localStorage.getItem("Cities"));
+    // Variable for user-selected city used in Ajax calls
+    var city = [];
+
+    // Variable for UV Ajax call URL
+    var uvURL = "";
+
+    //var cityArr =(localStorage.setItem("cities", cityArr));
 
     // Variable stating index of city in cityArr
     var cityIndex = "";
-   
-    //var city = "";
-    //
+
+    // Today's date
+    var today = new Date();
+
+    // Variable date for 5 day forecast (day)
+    var fiveDay = "";
+
+    // Variable date for 5 day forecast (month)
+    var fiveMonth = "";
+
+    // Variable date for 5 day forecast (year)
+    var fiveYear = "";
+
+    var date = {
+            day: today.getDate(),
+            month: (today.getMonth() + 1),
+            year: today.getUTCFullYear()
+        }
+        // Formatted today's date
+    var todayDate = date.month + "/" + date.day + "/" + date.year;
+    // function to display clickable buttons with city names in aside tag
+    function displayBtns() {
+
+        $("#button-container").empty();
+
+        for (i = 0; i < cityArr.length; i++) {
+
+            // Creates city button for aside tag
+            var cityBtn = $("<button>");
+
+            // Creates city button remove selector 'x'
+            var removeBtn = $("<i>");
+
+            cityBtn.addClass("city-buttons btn");
+
+            removeBtn.addClass("fa fa-times");
+
+            removeBtn.attr("aria-hidden", "true");
+
+            cityBtn.attr("data-name", cityArr[i]);
+
+            removeBtn.attr("data-name", cityArr[i]);
+
+            cityBtn.text(cityArr[i]);
+
+            cityBtn.append(removeBtn);
+
+            $("#button-container").append(cityBtn);
+
+        }
+
+    }
+    // adding addeventlistener to search button
     var searchButton = document.getElementById("search-button");
-    //
+
     searchButton.addEventListener("click", displayWeather);
 
+    // function that makes city-specific Ajax calls for current conditions and UV index
     function displayWeather(event) {
+
         event.preventDefault();
+
+
         var city = $("#city").val().trim();
 
         // first ajax call to get city data
@@ -26,41 +82,62 @@ $(document).ready(function () {
         $.ajax({
             url: queryURL,
             method: "GET"
-        }).then(function (response) {
+        }).then(function(response) {
             console.log(response);
 
             var img = $("<img>")
+
             var iconurl = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+
             img.attr("src", iconurl)
-            $("#weather-display").append(img);
-            $(".name").text(response.name);
-            $(".date").text(new Date(response.dt * 1000).toLocaleDateString())
+
+            img.attr("id", "today-head-container")
+
+            var cityDate = $("<h3>");
+
+            cityDate.attr("id", "weather-psiplay");
+
+            $("#name").append(img);
+
+            $("#date").text(response.name + " " + "(" + new Date(response.dt * 1000).toLocaleDateString() + ")");
+
+            // $("#date").text(response.main.temp);
+
+            $("#today-temp").text(response.main.temp + "" + "°F");
+
+            $("#today-humidity").text(response.main.humidity + "" + "%");
+
+            $("#wind").text(response.wind.speed + "" + "MPH");
+
+            $("#uv").text(response.main.temp);
 
 
-       
-            //queryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?" + response.city + "&appid=" + apikey + "&units=imperial"
-            console.log(response.coord.lon,response.coord.lat)
-            //queryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + "&lon="+ "&appid=" + apikey
 
-        queryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + response.coord.lat+ "&lon="+ response.coord.lon + "&appid=" + apikey
-                $.ajax({
-                    url: queryURL,
-                    method: "GET"
-                }).then(function (response) {
-                    console.log(response);
-        
-                    iconurl = "http://openweathermap.org/img/w/" + response.weather.main + ".png";
-                    img.attr("src", iconurl)
-                    $("#weather-display").append(img);
-                    $(".main").text(response.main);
-                    $(".date").text(new Date(response.dt * 1000).toLocaleDateString())
-                    
-                    //uvURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIkey + "&lat=" + response.coord.lat + "&lon=" + response.coord.lon;
-        
-                  //  queryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?" + response.coord.name + "&appid=" + apikey
-                })
+            var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid" + apikey + "&units=imperial"
+
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function(response1) {
+                console.log(response1);
+
+                var iconurl = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+                img.attr("src", iconurl)
+                $("#weather-display").append(img);
+                $("#name").text(response.name);
+                // $("#date").text(response.main.temp);
+                $("#today-temp").text(response.main.temp + "" + "F");
+                $("#today-humidity").text(response.main.humidity + "" + "%");
+                $("#wind").text(response.wind.speed + "" + "MPH");
+                $("#uv").text(response.main.temp);
+                $("#date").text(new Date(response.dt * 1000).toLocaleDateString())
+
+
             })
+
+            //})
+        })
 
     };
 
-});
+})
